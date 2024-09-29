fe8023e1be12c3815c7311fbfd50d6e19226eb44
Pyrospower <thepassione7@gmail.com>
2024-09-29 03:44:33 +0200

    fix: prettier config

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,11 +1,15 @@
-import baseConfig from "../prettier.config.mjs";
-
 /**
  * @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions &
  *       import("@ianvs/prettier-plugin-sort-imports").PluginConfig}
  */
 const config = {
-  ...baseConfig,
+  arrowParens: "always",
+  printWidth: 80,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
   plugins: [
     "@ianvs/prettier-plugin-sort-imports",
     "prettier-plugin-astro",
